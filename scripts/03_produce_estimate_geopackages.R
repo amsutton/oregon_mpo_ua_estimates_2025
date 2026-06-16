a39ccf2967a04d44eb9de94f399c60543d0187f5
amsutton <ajamsutton@gmail.com>
2026-06-16 13:33:39 -0700

    update with more intuitive gpkg build

--- scripts/03_produce_estimate_geopackages.R
+++ scripts/03_produce_estimate_geopackages.R
@@ -33,6 +33,7 @@
   blocks = st_transform(blocks,6558)
   
   counties=st_read(here('data/clean_counties.gpkg'))
+  counties = st_transform(counties,6558)
   
   mpos = st_read(here('data/clean_mpo_boundaries.gpkg'))
   mpos = mpos %>%
@@ -49,13 +50,15 @@
                                 str_detect(mpo_name,"Salem-Keizer") ~ "Salem-Keizer",
                                 str_detect(mpo_name,"Salem/Keizer") ~ "Salem-Keizer",
                                 str_detect(mpo_name,"Rogue Valley") ~ "Rogue Valley",
-                                str_detect(mpo_name,"METRO") ~ "Portland Metro"))
+                                str_detect(mpo_name,"METRO") ~ "Portland Metro")) %>%
+    st_transform(6558)
   
   uas = st_read(here('data/clean_UA_boundaries.gpkg'))
   uas = uas %>%
     filter(year=="2025",
            str_detect(ua_name,'Albany|Bend|Corvallis|Eugene|Grants Pass|Medford|Portland|Salem')) %>%
-    mutate(year = "2020")
+    mutate(year = "2020") %>%
+    st_transform(6558)
 }
 
 #please note the st_write() command has append=FALSE to overwrite previous version of the gpkg! 
@@ -63,18 +66,13 @@
 #will error out the function call. append=TRUE will add the new version of the data to the old file
 #regardless of whether similar data exist (i.e. you can create duplicates this way, careful!).
 
-build_split_mpo_ua_geom_boundaries = function(blocks,mpos,uas,counties,yearid){
+build_split_mpo_ua_geom_boundaries = function(blocks,mpos,uas,counties,year_of_estimates){
   
   if (st_crs(mpos) != st_crs(uas)) stop("st_crs(mpos) != st_crs(uas)")
   if (st_crs(mpos) != st_crs(blocks)) stop("st_crs(mpos) != st_crs(blocks)")
   if (st_crs(mpos) != st_crs(counties)) stop("st_crs(mpos) != st_crs(counties)")
   
-  blocks = blocks %>%
-    filter(year==yearid) %>%
-    st_as_sf()
-  counties = counties %>%
-    filter(year==yearid) %>%
-    st_as_sf()
+
   uas = uas %>%
     filter(year=="2020") %>%
     st_as_sf()
@@ -86,12 +84,32 @@ build_split_mpo_ua_geom_boundaries = function(blocks,mpos,uas,counties,yearid){
     if(FALSE %in% st_is_valid(mpos)) mpos = st_make_valid(mpos)
     if(FALSE %in% st_is_valid(mpos)) stop("invalid MPO geometry persists")
     
+  yearid = c('2000','2010','2020')
+
+  mpo_totals = data.frame()
+  mpo_by_co = data.frame()
+
+  for(i in 1:length(yearid)){
+    print(yearid[i])
+    
+    temp_blocks = blocks %>%
+      filter(year==yearid[i]) %>%
+      st_as_sf()
+    temp_counties = counties %>%
+      filter(year==yearid[i]) %>%
+      st_as_sf()
+    
     #blocks in MPO boundaries
-    mpo_totals = st_join(mpos,blocks)
+    mpo_totals2 = st_join(mpos,temp_blocks)
+    mpo_totals = rbind(mpo_totals,mpo_totals2)
     
     #blocks in county boundaries
-    blocks_in_mpos = st_intersection(mpos,blocks)
-    mpo_by_co = st_join(counties,blocks_in_mpos)
+    blocks_in_mpos = st_intersection(mpos,temp_blocks)
+    mpo_by_co2 = st_join(temp_counties,blocks_in_mpos)
+    
+    mpo_by_co = rbind(mpo_by_co,mpo_by_co2)
+    
+  }
     
     mpo_by_co %>%
       rename(year=year.x) %>%
@@ -100,11 +118,11 @@ build_split_mpo_ua_geom_boundaries = function(blocks,mpos,uas,counties,yearid){
       group_by(year,mpo_name,cname) %>%
       reframe(estimate = sum(value),
               geom) %>% 
-      distinct() %>%
+      distinct() %>% 
       st_as_sf() %>%
       st_collection_extract(type = "POLYGON") %>%
       st_transform(4326) %>%
-      st_write(here(paste0('data/clean_mpo_ua_joint_boundaries/mpo_by_co_',yearid,'.gpkg')),append=FALSE)
+      st_write(here(paste0('data/clean_mpo_ua_joint_boundaries/mpo_by_co_',year_of_estimates,'.gpkg')),append=FALSE)
     
     mpo_totals %>%
       rename(year=year.x) %>%
@@ -117,16 +135,36 @@ build_split_mpo_ua_geom_boundaries = function(blocks,mpos,uas,counties,yearid){
       st_as_sf() %>%
       st_collection_extract(type = "POLYGON") %>%
       st_transform(4326) %>%
-      st_write(here(paste0('data/clean_mpo_ua_joint_boundaries/mpo_totals_',yearid,'.gpkg')),append=FALSE)
+      st_write(here(paste0('data/clean_mpo_ua_joint_boundaries/mpo_totals_',year_of_estimates,'.gpkg')),append=FALSE)
 
-    #blocks in UA boundaries
-    ua_totals = st_join(uas,blocks)
-
-    #blocks in county boundaries
-    blocks_in_uas = st_intersection(uas,blocks)
-    ua_by_co = st_join(counties,blocks_in_uas)
 
     
+    ua_totals = data.frame()
+    ua_by_co = data.frame()
+    
+    for(i in 1:length(yearid)){
+      print(yearid[i])
+      
+      temp_blocks = blocks %>%
+        filter(year==yearid[i]) %>%
+        st_as_sf()
+      temp_counties = counties %>%
+        filter(year==yearid[i]) %>%
+        st_as_sf()
+      
+      #blocks in ua boundaries
+      ua_totals2 = st_join(uas,temp_blocks)
+      ua_totals = rbind(ua_totals,ua_totals2)
+      
+      #blocks in county boundaries
+      blocks_in_uas = st_intersection(uas,temp_blocks)
+      ua_by_co2 = st_join(temp_counties,blocks_in_uas)
+      
+      ua_by_co = rbind(ua_by_co,ua_by_co2)
+      
+    }
+    
+   
     ua_totals %>%
       rename(year=year.x) %>%
       select(year,ua_name,value,geom) %>%
@@ -138,7 +176,7 @@ build_split_mpo_ua_geom_boundaries = function(blocks,mpos,uas,counties,yearid){
       st_as_sf() %>%
       st_collection_extract(type = "POLYGON") %>%
       st_transform(4326) %>%
-      st_write(here(paste0('data/clean_mpo_ua_joint_boundaries/ua_totals_',yearid,'.gpkg')),append=FALSE)
+      st_write(here(paste0('data/clean_mpo_ua_joint_boundaries/ua_totals_',year_of_estimates,'.gpkg')),append=FALSE)
     
     ua_by_co %>%
       rename(year=year.x) %>%
@@ -151,26 +189,24 @@ build_split_mpo_ua_geom_boundaries = function(blocks,mpos,uas,counties,yearid){
       st_as_sf() %>%
       st_collection_extract(type = "POLYGON") %>%
       st_transform(4326) %>%
-      st_write(here(paste0('data/clean_mpo_ua_joint_boundaries/ua_by_co_',yearid,'.gpkg')),append=FALSE)
+      st_write(here(paste0('data/clean_mpo_ua_joint_boundaries/ua_by_co_',year_of_estimates,'.gpkg')),append=FALSE)
   
 }
 
 
 #N.B.: If during the below loop you see that the geometry type after writing each
-#gpkg is Point, or especially Unknown, you have an issue! 
+#gpkg is Point, or especially Unknown, you may have an issue! 
 #Look into geometry validity (st_is_valid)
 #of the geometries used to create the new objects, and also consider whether
 #there are "slivers" or similar nonsense geometries (can usually be dealt with)
 #in combination with searching for them, and 
 #using st_collection_extract(x,type = "POLYGON")
 
+year_of_estimates = "2025"
 
-years = c("2000","2010","2020")
+#this could be made to be more memory efficient, but it isn't bad
+build_split_mpo_ua_geom_boundaries(blocks,mpos,uas,counties,year_of_estimates)
 
-for (i in 1:length(years)){
-  print(years[i])
-  build_split_mpo_ua_geom_boundaries(blocks,mpos,uas,counties,yearid=years[i])
-}
 
 
 
@@ -210,6 +246,6 @@ for (i in 1:length(temp)){
 
 #looks good; crs = 4326 for simplicity
 st_layers(here('results/clean/2025_oregon_mpo_ua_boundaries.gpkg'))
-temp = st_read(here('data/clean_mpo_ua_joint_boundaries/mpo_by_co_2020.gpkg'),layer='mpo_by_co_2020')
+temp = st_read(here('results/clean/2025_oregon_mpo_ua_boundaries.gpkg'),layer='mpo_by_co_2025')
 glimpse(temp)
 
